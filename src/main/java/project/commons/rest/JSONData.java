190b9e121a852dec10311b1dfefab4a1e1398245
daeyounggg <kdyzab@gmail.com>
2023-11-10 15:17:07 +0900

    설정 중...

---
+++ src/main/java/project/commons/rest/JSONData.java
@@ -0,0 +1,12 @@
+package project.commons.rest;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class JSONData<T> {
+    private boolean success = true;
+    private HttpStatus status = HttpStatus.OK;
+    private T data;
+    private String message;
+}
