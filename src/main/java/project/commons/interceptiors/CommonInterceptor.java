c65e18546e71edbfb300394ba253f2959d7b0743
daeyounggg <kdyzab@gmail.com>
2023-11-13 18:17:43 +0900

    PC, Mobile 분리

--- src/main/java/project/commons/interceptiors/CommonInterceptor.java
+++ src/main/java/project/commons/interceptiors/CommonInterceptor.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import project.commons.Utils;
 
 @Component
 public class CommonInterceptor implements HandlerInterceptor {
@@ -25,5 +26,11 @@ public class CommonInterceptor implements HandlerInterceptor {
         }
 
         /* PC, Mobile 수동 변경 처리 E */
+
+        /* 로그인 페이지 아닐 경우 로그인 유효성 검사 세션 삭제 처리 */
+        String URI = request.getRequestURI();
+       if (URI.indexOf("/member/login") == -1){
+           Utils.loginInit(session);
+       }
     }
 }
