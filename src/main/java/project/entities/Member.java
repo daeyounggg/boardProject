b56c7ac01e41e7bc5d616c0fe9aff63587868619
daeyounggg <kdyzab@gmail.com>
2023-11-15 18:19:36 +0900

    JPA

--- src/main/java/project/entities/Member.java
+++ src/main/java/project/entities/Member.java
@@ -1,29 +1,56 @@
 package project.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import project.commons.constants.MemberType;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @Entity
+@NoArgsConstructor @AllArgsConstructor
+@Table(indexes = {
+    @Index(name="idx_member_userNm", columnList = "userNm"),
+    @Index(name="idx_member_mobile", columnList = "mobile")
+})
+
 public class Member {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userNo;
 
+    @Column(length = 65, unique = true, nullable = false)
     private String email;
 
+    @Column(length = 65, name="pw", nullable = false)
     private String password;
 
+    @Column(length = 40, nullable = false)
     private String userNm;
 
+    @Column(length = 11)
     private String mobile;
 
+    @Column(length = 10, nullable = false)
+    @Enumerated(EnumType.STRING)
     private MemberType mtype = MemberType.USER;
 
+    @Column(updatable = false)
+    @CreationTimestamp
     private LocalDateTime regDt;
+
+    @Column(insertable = false)
+    @UpdateTimestamp
     private LocalDateTime modDt;
+
+    /*
+    @Temporal()
+    private Date date;
+     */
 }
--- src/test/java/project/jpaex/Ex01.java
+++ src/test/java/project/jpaex/Ex01.java
@@ -2,26 +2,58 @@ package project.jpaex;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import project.commons.constants.MemberType;
 import project.entities.Member;
 
 @SpringBootTest
 @Transactional
-@TestPropertySource(locations = "classpath:application-test.yml")
+//@TestPropertySource(activeProfiles="test")
 public class Ex01 {
 
     @PersistenceContext
     private EntityManager em;
 
+    @BeforeEach
+    void init(){
+        Member member = new Member();
+        //member.setUserNo(1L);
+        member.setEmail("user01@test.org");
+        member.setUserNm("사용자01");
+        member.setPassword("123456");
+        member.setMobile("01000000000");
+        member.setMtype(MemberType.USER);
+
+        em.persist(member); // 변화 감지 상태
+        em.flush();
+        em.clear(); // 영속성 비우기
+    }
+
+    @Test
+    void test2(){
+        Member member = em.find(Member.class, 1L); // DB -> 영속성 컨텍스트로 추가
+        System.out.println(member);
+
+        Member member2 = em.find(Member.class, 1L); // 영속성 컨텍스트 -> 조회
+        System.out.println(member2);
+
+        TypedQuery<Member> query = em.createQuery("SELECT m FROM Users As m WHERE m.email LIKE :key", Member.class);
+        query.setParameter("key","%user%");
+        Member member3 = query.getSingleResult();
+
+        member3.setUserNm("(수정)사용자01");
+        em.flush();
+    }
+
     @Test
     void test1(){
         Member member = new Member();
-        member.setUserNo(1L);
         member.setEmail("user01@test.org");
+        //member.setUserNo(1L);
         member.setUserNm("사용자01");
         member.setPassword("123456");
         member.setMobile("01000000000");
@@ -40,5 +72,10 @@ public class Ex01 {
 
         //em.remove(member);
         //em.flush();
+    }
+
+    @Test
+    void test3(){
+
     }
 }
