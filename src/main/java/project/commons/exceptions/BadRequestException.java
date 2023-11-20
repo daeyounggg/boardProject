f3a673ea93fb283634ba9359307616e9e5701494
daeyounggg <kdyzab@gmail.com>
2023-11-20 17:54:32 +0900

    기본 설정 중

---
+++ src/main/java/project/commons/exceptions/BadRequestException.java
@@ -0,0 +1,13 @@
+package project.commons.exceptions;
+
+import project.commons.Utils;
+
+public class BadRequestException extends AlertBackException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException() {
+        super(Utils.getMessage("BadRequest", "error"));
+    }
+}
