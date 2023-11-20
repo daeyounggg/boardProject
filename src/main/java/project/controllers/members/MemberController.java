0b29ef923f999f8f293bf1b987d38f76ec5cfec6
daeyounggg <kdyzab@gmail.com>
2023-11-20 17:04:54 +0900

    기본 설정 중

--- src/main/java/project/controllers/members/MemberController.java
+++ src/main/java/project/controllers/members/MemberController.java
@@ -1,28 +1,24 @@
 package project.controllers.members;
 
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import project.commons.MemberUtil;
 import project.commons.Utils;
-import project.entities.BoardData;
+import project.entities.Member;
 
 @Slf4j
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
-@Transactional
 public class MemberController {
 
     private final Utils utils;
     private final MemberUtil memberUtil;
-    private final EntityManager em;
 
     @GetMapping("/join")
     public String join() {
@@ -32,23 +28,38 @@ public class MemberController {
 
     @GetMapping("/login")
     public String login(String redirectURL, Model model) {
+
         model.addAttribute("redirectURL", redirectURL);
+
         return utils.tpl("member/login");
     }
 
     @ResponseBody
     @GetMapping("/info")
-    public void info(){
-        BoardData data = BoardData.builder()
-                .subject("제목")
-                .content("내용")
-                .build();
+    public void info() {
 
-        em.persist(data);
-        em.flush();
+        Member member = memberUtil.getMember();
+        if (memberUtil.isLogin()) {
+            log.info(member.toString());
+        }
 
-        data.setSubject("(수정)제목");
-        em.flush();
+        log.info("로그인 여부 : {}", memberUtil.isLogin());
     }
+    /*
+    public void info() {
+        MemberInfo member = (MemberInfo)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+        log.info(member.toString());
+    }
+    */
+    /*
+    public void info(@AuthenticationPrincipal MemberInfo memberInfo) {
+        log.info(memberInfo.toString());
+    */
+    /*
+    public void info(Principal principal) {
+        String email = principal.getName();
+        log.info(email);
+    }
+     */
 }
